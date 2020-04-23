1fa35dc11a3ce39b82932601f3f36301209c35a3
Mruda715 <60145061+Mruda715@users.noreply.github.com>
2020-04-23 08:33:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const onNameClick = function () {
+  //  console.log('got into function')
+  //   $('#message').text('Welcome to the moon.')
+  //   $('#message').css('background-Color', 'blue')
+  //   setTimeout(() => $('#message').text(''), 3000)
+  //   $('.container').addClass('animated rollOut');
+  //   $('#welcomePage').addClass('animated rollIn')
+  }
